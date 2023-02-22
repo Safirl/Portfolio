8fa4e1815a079a4ce2b2d94e4b605e22a053854d
Safirl <loic.leforestier@mmibordeaux.com>
2023-02-23 00:05:34 +0100

    ajout de la nav bar et création CSS

---
+++ code/header.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Loïc Leforestier - Portfolio</title>
+    <link rel="stylesheet" href="../css/reset.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Sono:wght@200;300;400;500;600;700;800&display=swap" rel="stylesheet">
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous"> -->
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/navbar.css">
+</head>
+
+<body>
+    <!-- As a link -->
+    <nav class="navbar">
+            <ul class="navbar-boutons">
+                <li class="nav-item">
+                    <a class="nav-button" aria-current="page" href="#">Accueil</a>
+                </li>
+                <li class="nav-button">
+                    <a class="nav-link" href="#">Développement</a>
+                </li>
+                <li class="nav-button">
+                    <a class="nav-link" href="#">Films</a>
+                </li>
+                <li class="nav-button">
+                    <a class="nav-link" href="#">Galerie</a>
+                </li>
+                <li class="nav-button">
+                    <a class="nav-link" href="#">Contact</a>
+                </li>
+            </ul>
+    </nav>
+<script src=""></script>
+</body>
+</html>
+
+
---
+++ code/index.php
@@ -0,0 +1,9 @@
+<body>
+    <?php require_once'header.php';?>
+    
+
+
+
+    <?php require_once'footer.php'; ?>
+</body>
+</html>
