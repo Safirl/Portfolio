9bd843afcd3ecfb409b29454a6c1fc83f04faa0d
Safirl <loic.leforestier@mmibordeaux.com>
2023-02-25 18:43:53 +0100

    Multiples changements personnels

--- code/index.php
+++ code/index.php
@@ -1,9 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Loïc Leforestier - Portfolio</title>
+    <link rel="stylesheet" href="../css/reset.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Sono:wght@200;300;400;500;600;700;800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/main.css">
+    <link rel="stylesheet" href="../css/navbar.css">
+</head>
 <body>
-    <?php require_once'header.php';?>
-    
-
-
-
-    <?php require_once'footer.php'; ?>
+    <?php require_once'header.html';
+    require_once'main.html';
+    require_once'footer.html';?>
 </body>
 </html>
