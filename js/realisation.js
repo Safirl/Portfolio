047b681d50eca3058de30d1c812f887033fad4f6
loic leforestier <loic.leforestier@mmibordeaux.com>
2023-04-04 20:07:33 +0200

    Adding developpement realisation and responsive

---
+++ js/realisation.js
@@ -0,0 +1,15 @@
+const REALISATIONS = [
+  {
+    title: 'Développement',
+    text: 'lorem ipsum',
+    images: {
+      1 : '/assets/LEFORESTIER_Loïc_PhotoPerso.jpg',
+      2 : ''
+    }
+
+  }
+];
+
+REALISATIONS.forEach(Element => {
+
+});
